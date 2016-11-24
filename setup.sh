1ca1f603a7c8983d6aa15838dc7745276c48e421
Dennis Ideler <ideler.dennis@gmail.com>
2016-11-23 22:10:07 -0400

    Clone repo with https instead of ssh
    To avoid the fingerprint prompt.

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ apt update
 apt install git -y
 
 mkdir -p ~/github.com
-git clone git@github.com:dideler/setup-ubuntu.git ~/github.com/dideler/setup-ubuntu
+git clone https://github.com/dideler/setup-ubuntu.git ~/github.com/dideler/setup-ubuntu
 
 pushd ~/github.com/dideler/setup-ubuntu
   rake --trace
