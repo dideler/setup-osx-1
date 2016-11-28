bd6ff31622cb4241278d5c6d4bb69d3c913b6b75
Dennis Ideler <ideler.dennis@gmail.com>
2016-11-28 16:26:36 -0400

    Wrap skype's repository in double quotes
    So add-apt-repository sees it as a single argument.

--- applications/default.rb
+++ applications/default.rb
@@ -8,7 +8,7 @@ APPLICATIONS = []
   'fish' => 'ppa:fish-shell/release-2',
   'fluxgui' => 'ppa:nathan-renniewaldock/flux',
   'git' => 'ppa:git-core/ppa',
-  'skype' => 'deb http://archive.canonical.com/ $(lsb_release -sc) partner',
+  'skype' => '"deb http://archive.canonical.com/ $(lsb_release -sc) partner"',
   'default-jdk htop postgresql postgresql-contrib silversearcher-ag tree unzip vim vlc wget' => nil,
 }.each do |packages, ppa|
   apt_install(packages, ppa)
