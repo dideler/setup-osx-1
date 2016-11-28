590198fb45312d146e553b6b4c64d80318f4cb01
Dennis Ideler <ideler.dennis@gmail.com>
2016-11-28 16:14:50 -0400

    Use bashrc instead of bash_profile for rbenv
    As suggested in rbenv's README.

--- package_managers/rbenv.rb
+++ package_managers/rbenv.rb
@@ -1,5 +1,5 @@
 desc 'Install rbenv'
 task 'package_manager:rbenv' => 'package_manager:linuxbrew' do
   system 'brew install rbenv ruby-build'
-  system 'echo "eval \"$(rbenv init -)\"" >> ~/.bash_profile'
+  system 'echo "eval \"$(rbenv init -)\"" >> ~/.bashrc'
 end
