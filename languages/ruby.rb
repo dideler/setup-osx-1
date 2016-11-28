3663c0761354d01b8281776b1bce67bbbade01d5
Dennis Ideler <ideler.dennis@gmail.com>
2016-11-28 10:09:43 -0400

    Fix ruby install
    Need to start a new login shell because rbenv won't exist in current
    shell instance as bash_profile hasn't been explicitly sourced.

--- languages/ruby.rb
+++ languages/ruby.rb
@@ -1,7 +1,7 @@
 desc 'Setup ruby'
 task 'language:ruby' => 'package_manager:rbenv' do
   unless File.exist?(File.expand_path('~/.rbenv/versions/2.3.3'))
-    system 'rbenv install 2.3.3'
+    system 'bash -lc "rbenv install 2.3.3"'
   end
   File.open(File.expand_path('~/.gemrc'), 'w').write('gem: --no-document')
 end
