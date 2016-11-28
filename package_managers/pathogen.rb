344be8e4de530d55b1630fca97e82733aa02c3ff
Dennis Ideler <ideler.dennis@gmail.com>
2016-11-28 10:01:18 -0400

    Remove unused installations
    Some of these applications don't need their own installation script as
    they can be installed easily via apt. Others I don't use or are easier
    to install via the browser (in future could download .deb and use dpkg).

--- package_managers/pathogen.rb
+++
@@ -1,5 +0,0 @@
-task 'package_manager:pathogen' do
-  system 'mkdir -p ~/.nvim/autoload'
-  system 'mkdir -p ~/.nvim/bundle'
-  system 'curl -LSso ~/.nvim/autoload/pathogen.vim https://tpo.pe/pathogen.vim'
-end
