8676aac6128aeef3c61bf07cd3787cbdc9fd900b
Dennis Ideler <ideler.dennis@gmail.com>
2016-12-02 10:32:00 -0400

    Use bash-it's rbenv plugin
    In case instructions ever change for how to init rbenv, bash-it's plugin
    will now handle it instead of me having to update the instructions.

--- package_managers/rbenv.rb
+++ package_managers/rbenv.rb
@@ -2,5 +2,5 @@ desc 'Install rbenv'
 task 'package_manager:rbenv' => 'package_manager:linuxbrew' do
   system 'bash -lc "brew install rbenv"'
   system 'bash -lc "brew install ruby-build"'
-  system 'echo "eval \"$(rbenv init -)\"" >> ~/.bashrc'
+  system 'bash -lc "bash-it enable plugin rbenv"'
 end
