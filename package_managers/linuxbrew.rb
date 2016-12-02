0394449fe2111c3a3a08312605c0cbe5e6101005
Dennis Ideler <ideler.dennis@gmail.com>
2016-12-02 10:54:20 -0400

    Load env vars via bash-it

--- package_managers/linuxbrew.rb
+++ package_managers/linuxbrew.rb
@@ -4,9 +4,14 @@ task 'package_manager:linuxbrew' do
     system('brew update')
   else
     system('ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"')
-    system('echo "export PATH=\"$HOME/.linuxbrew/bin:$PATH\"" >>~/.bashrc')
-    system('echo "export MANPATH=\"$HOME/.linuxbrew/share/man:$MANPATH\"" >>~/.bashrc')
-    system('echo "export INFOPATH=\"$HOME/.linuxbrew/share/info:$INFOPATH\"" >>~/.bashrc')
+
+    File.open(File.expand_path('~/.bash_it/custom/linuxbrew.env.bash'), 'w').write(
+<<-EOS
+export PATH="$HOME/.linuxbrew/bin:$PATH"
+export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"
+export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"
+EOS
+    )
   end
 end
 
