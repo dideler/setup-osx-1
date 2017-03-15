f4ede6035d0ccd93fa34cb0b5593d0db2b86fa8a
Dennis Ideler <ideler.dennis@gmail.com>
2017-03-15 11:01:36 +0000

    Install ruby build dependencies

--- languages/ruby.rb
+++ languages/ruby.rb
@@ -2,6 +2,7 @@ desc 'Setup ruby'
 task 'language:ruby' => 'package_manager:rbenv' do
   puts 'Installing ruby'
   unless File.exist?(File.expand_path('~/.rbenv/versions/2.3.3'))
+    system 'bash -lc "sudo apt install -y libreadline-dev zlib1g-dev"'
     system 'bash -lc "rbenv install 2.3.3"'
   end
   File.open(File.expand_path('~/.gemrc'), 'w').write('gem: --no-document')
