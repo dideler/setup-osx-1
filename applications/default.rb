725a26fc66965ee67f2af8a732828092e1f43b8e
Dennis Ideler <ideler.dennis@gmail.com>
2017-03-15 09:21:00 +0000

    Install thunderbird and pdftk

--- applications/default.rb
+++ applications/default.rb
@@ -9,7 +9,8 @@ APPLICATIONS = []
   'fluxgui' => 'ppa:nathan-renniewaldock/flux',
   'git' => 'ppa:git-core/ppa',
   'skype' => '"deb http://archive.canonical.com/ $(lsb_release -sc) partner"',
-  'default-jdk htop postgresql postgresql-contrib silversearcher-ag tree unzip vim vlc wget' => nil,
+  'thunderbird' => 'ppa:ubuntu-mozilla-daily/ppa',
+  'default-jdk htop pdftk postgresql postgresql-contrib silversearcher-ag tree unzip vim vlc wget' => nil,
 }.each do |packages, ppa|
   apt_install(packages, ppa)
   APPLICATIONS.push(packages)
